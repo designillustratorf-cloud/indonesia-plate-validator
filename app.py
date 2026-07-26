2b3e7ac6779f8874496dbe1b2b2976790d580328
muhammad faris <designillustratorf@gmail.com>
2026-07-26 10:21:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -124,6 +124,15 @@ def api_validate():
 
 import os
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Railway OK"
+
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
